ff00960dafab2af8326c881ccf5e5a95c07e9866
Konrad <konradmy@gmail.com>
2021-08-28 19:38:33 +0200

    Add file for building and pushing to pypi.

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="typedb_query_builder_test",
+    version="0.0.3",
+    author="Konrad Mysliwiec",
+    author_email="konradmy@gmail.com",
+    description="A lightweight package for building TypeDB queries.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/konradmy/typedb-query-builder",
+    project_urls={
+        "Bug Tracker": "https://github.com/konradmy/typedb-query-builder/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    # package_dir={"": "typedb_query_builder"},
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+)
